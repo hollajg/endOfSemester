5e05ef85555ca83a44d97d2f183892130f5d9297
hollajg <59853819+hollajg@users.noreply.github.com>
2020-05-04 15:20:22 -0400

    Add files via upload

---
+++ Fizzbuzz.py
@@ -0,0 +1,13 @@
+#Creating a Fizzbuzz
+#ISAT 252
+#Jackson Holland
+
+for num in range(1, 101):
+    if num % 15 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
